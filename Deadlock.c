074368f67f68e6cd22196eea288ffe2a9ecb6279
pradeepta3456 <pratimahub323@gmail.com>
2026-07-03 08:33:13 +0545

    Add Deadlock.c

---
+++ Deadlock.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+pthread_mutex_t lockX = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t lockY = PTHREAD_MUTEX_INITIALIZER;
+
+void *safe_transfer_a(void *arg) {
+    (void)arg;
+    pthread_mutex_lock(&lockX);
+    usleep(1000);
+    pthread_mutex_lock(&lockY);
+    printf("[safe_transfer_a] acquired X then Y safely.\n");
+    pthread_mutex_unlock(&lockY);
+    pthread_mutex_unlock(&lockX);
+    return NULL;
+}
+
+void *safe_transfer_b(void *arg) {
+    (void)arg;
+    /* Same order as thread A: X before Y. This is the fix. */
+    pthread_mutex_lock(&lockX);
+    usleep(1000);
+    pthread_mutex_lock(&lockY);
+    printf("[safe_transfer_b] acquired X then Y safely.\n");
+    pthread_mutex_unlock(&lockY);
+    pthread_mutex_unlock(&lockX);
+    return NULL;
+}
+
+int main(void) {
+    printf("=== Part B: Deadlock prevention via fixed lock ordering ===\n");
+
+    pthread_t tA, tB;
+    pthread_create(&tA, NULL, safe_transfer_a, NULL);
+    pthread_create(&tB, NULL, safe_transfer_b, NULL);
+    pthread_join(tA, NULL);
+    pthread_join(tB, NULL);
+
+    return 0;
+}
